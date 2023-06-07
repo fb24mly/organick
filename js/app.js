c5d0f23befafb1bdf5ae2bf5bbf1b9bc19bf6baf
FB24M <103760918+iFB24M@users.noreply.github.com>
2023-06-07 10:25:37 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/scripts/script.js
+let starsContainers = document.querySelectorAll('[data-stars]');
+
+starsContainers.forEach(starsContainer => {
+	for(let i = 0; i < Number(starsContainer.dataset.stars); i++) {
+		starsContainer.insertAdjacentHTML('beforeend', '<img src="img/star.svg" alt="Rate Star" class="product-card__star">');
+	}
+});
+;// CONCATENATED MODULE: ./src/js/app.dev.js
+
+
+
+/******/ })()
+;
